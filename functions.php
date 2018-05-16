47601c439dd14789c5ed3e0a57113fa4cc2fb798
Mdsujansarkar <sujansarkar786@gmail.com>
2018-05-17 00:35:41 +0600

    add and remove

--- functions.php
+++ functions.php
@@ -643,8 +643,15 @@ function woo_after(){ ?>
 
   		<?php } 		
 
+//action remove
 
+remove_action('woocommerce_before_shop_loop_item_title','woocommerce_show_product_loop_sale_flash');
+remove_action('woocommerce_before_shop_loop_item_title','woocommerce_template_loop_product_thumbnail');
 
+remove_action('woocommerce_before_shop_loop_item','woocommerce_template_loop_product_link_open');
+remove_action('woocommerce_after_shop_loop_item','woocommerce_template_loop_product_link_close');
+
+// add action 
 
 
 
